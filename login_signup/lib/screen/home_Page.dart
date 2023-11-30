0ea406a603e23bb7ab27ed08b56d4529f38638e8
MinJaehyun <43669992+MinJaehyun@users.noreply.github.com>
2023-11-30 23:25:53 +0900

    update home_page

--- login_signup/lib/screen/home_Page.dart
+++ login_signup/lib/screen/home_Page.dart
@@ -8,11 +8,19 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _formKey = GlobalKey<FormState>();
   bool isSignup = false; // login 상태
-  // bool isSignup = true; // signup 상태
+  var password = '';
 
   @override
   Widget build(BuildContext context) {
+    void _tryValidator() {
+      var valid = _formKey.currentState?.validate();
+      if (valid == true) {
+        _formKey.currentState?.save();
+      }
+    }
+
     return Scaffold(
       body: SingleChildScrollView(
         child: Container(
@@ -37,8 +45,20 @@ class _HomePageState extends State<HomePage> {
                           child: Column(
                             children: [
                               SizedBox(height: 150),
-                              TextButton(onPressed: () {}, child: Text('로그인')),
-                              TextButton(onPressed: () {}, child: Text('회원가입')),
+                              TextButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      isSignup = false;
+                                    });
+                                  },
+                                  child: Text('로그인')),
+                              TextButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      isSignup = true;
+                                    });
+                                  },
+                                  child: Text('회원가입')),
                               TextButton(onPressed: () {}, child: Text('튜토리얼')),
                               TextButton(onPressed: () {}, child: Text('밝기조절')),
                             ],
@@ -74,24 +94,40 @@ class _HomePageState extends State<HomePage> {
                                     child: Container(
                                       margin: EdgeInsets.only(top: 40),
                                       child: Form(
+                                        key: _formKey,
                                         child: Column(children: [
                                           TextFormField(
+                                            key: ValueKey(1),
+                                            validator: (value) {
+                                              if (value!.isEmpty || value.length < 4) {
+                                                return 'Please enter at least 4 characters';
+                                              }
+                                              return null;
+                                            },
                                             decoration: InputDecoration(
                                               hintText: 'id',
                                             ),
                                           ),
                                           SizedBox(height: 20),
                                           TextFormField(
+                                            key: ValueKey(2),
+                                            validator: (value) {
+                                              if (value!.isEmpty || value.length < 5) {
+                                                return 'Password must be at least characters long.';
+                                              }
+                                              return null;
+                                            },
                                             decoration: InputDecoration(
                                               hintText: 'password',
                                             ),
+                                            obscureText: true,
                                           ),
                                         ]),
                                       ),
                                     ),
                                   ),
                                 ),
-        
+
                               // note: Signup 화면
                               if (isSignup)
                                 Container(
@@ -99,37 +135,66 @@ class _HomePageState extends State<HomePage> {
                                     child: Container(
                                       margin: EdgeInsets.only(top: 40),
                                       child: Form(
+                                        key: _formKey,
                                         child: Column(children: [
                                           TextFormField(
+                                            key: ValueKey(3),
+                                            validator: (value) {
+                                              if (value!.isEmpty || value.length < 4) {
+                                                return 'Please enter at least 4 characters';
+                                              }
+                                              return null;
+                                            },
                                             decoration: InputDecoration(
                                               hintText: 'id',
                                             ),
                                           ),
                                           SizedBox(height: 20),
                                           TextFormField(
+                                            onChanged: (value) {
+                                              password = value;
+                                            },
+                                            key: ValueKey(4),
+                                            validator: (value) {
+                                              if (value!.isEmpty || value.length < 6) {
+                                                return 'Password must be at least characters long.';
+                                              }
+                                              return null;
+                                            },
                                             decoration: InputDecoration(
                                               hintText: 'password',
                                             ),
+                                            obscureText: true,
                                           ),
                                           SizedBox(height: 20),
                                           TextFormField(
+                                            key: ValueKey(5),
+                                            validator: (value) {
+                                              if (password != value) {
+                                                return 'Please enter the same password';
+                                              }
+                                              return null;
+                                            },
                                             decoration: InputDecoration(
                                               hintText: 'password 확인',
                                             ),
+                                            obscureText: true,
                                           ),
                                         ]),
                                       ),
                                     ),
                                   ),
                                 ),
-        
+
                               // note: 로그인/회원가입 버튼
                               Container(
                                 child: Row(
                                   mainAxisAlignment: MainAxisAlignment.end,
                                   children: [
                                     ElevatedButton.icon(
-                                      onPressed: () {},
+                                      onPressed: () {
+                                        _tryValidator();
+                                      },
                                       icon: Icon(Icons.info_outlined),
                                       label: Text('확인'),
                                     ),
@@ -144,7 +209,7 @@ class _HomePageState extends State<HomePage> {
                   ),
                 ),
               ),
-        
+
               // note: 하단 패널 - 미구현
               // Container(
               //   margin: EdgeInsets.all(25),
